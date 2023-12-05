a939dee4d978cdc2d52c350303fbbf4d7e59c065
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-12-05 10:04:30 -0500

    Fetch data from Contentful GraphQL api.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const query = `
+{
+  pageCollection {
+    items {
+      title
+      logo {
+        url
+      }
+    }
+  }
+}
+`;
+
+const { ACCESS_TOKEN, SPACE_ID } = process.env;
 
 function App() {
+  const [content, setContent] = useState(null);
 
+  useEffect(() => {
+    console.log("💅🏻 in useEffect");
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/?access_token=${ACCESS_TOKEN}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer JjxBmPXFxdVMU45d0BQ0OVO3KB3zATWxERqcmUxgGEo",
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then((json) => setContent(json.data));
+  }, []);
+
+  if (!content) {
+    return "Loading...";
+  } else {
+    console.log("🌸 content: ", content);
+  }
+
+  // render the fetched Contentful data
   return (
-    <>
-      <h1>Arnoldas Stramskas</h1>
-      <div className="card" style={{border: "2px solid hotpink"}}>
-      </div>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        {/* Render your content here */}
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
